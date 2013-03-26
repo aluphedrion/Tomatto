4236ce570cf0a0e2752d3025b23c1d817aecdf9b
Alfredo Becerra Bauzulli <freddi.blues@gmail.com>
2013-03-26 23:10:57 +0100

    Initial Commit

---
+++ Tomatto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tomatto
+//
+//  Created by bluezy on 26/03/13.
+//  Copyright (c) 2013 bluezy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tomatto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tomatto
+//
+//  Created by bluezy on 26/03/13.
+//  Copyright (c) 2013 bluezy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
